bffc5f65456953658b658ccab43049963a3382d7
Martin Qian <mackuntu@gmail.com>
2012-01-11 20:44:27 -0500

    added Action.java

---
+++ src/poker/Action.java
@@ -0,0 +1,17 @@
+package poker;
+
+public enum Action {
+	FOLD,
+	RAISE,
+	CALL,
+	CHECK;
+	protected int amount;
+	public void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
+	public int getAmount()
+	{
+		return amount;
+	}
+}
